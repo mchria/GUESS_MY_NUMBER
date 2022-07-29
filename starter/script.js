5b7debe3a12cc703752667445d21ef6839c27bf2
mchria <100881916+mchria@users.noreply.github.com>
2022-07-29 10:18:20 -0300

    Update script.js

--- starter/script.js
+++ starter/script.js
@@ -1,1 +1,25 @@
+
 'use strict';
+
+
+const number = Math.trunc(Math.random() * 20) + 1;
+// Math.trunc(Math.random() * 21)
+
+//document.querySelector('.message').textContent = 'Hello World!';
+//document.querySelector('.score').textContent = 42;
+document.querySelector('.number').textContent = number;
+
+
+
+document.querySelector('.check').addEventListener('click',
+    function () {
+
+        const guess = document.querySelector('.guess').value;
+
+        if (!guess) {
+            document.querySelector('.message').textContent = '💣No number';
+        }
+
+    }
+);
+
